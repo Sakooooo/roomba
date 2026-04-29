acf5003a5f3568155f2bc764b63d2fc0bfc497df
Sakoooooo <78461130+Sakooooo@users.noreply.github.com>
2026-04-29 10:42:32 +0300

    Oh wow look at that i made state

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,49 @@
-use iced::widget::{button, text};
+use iced::widget::{button, container, text};
 use iced::Element;
 
 mod views;
 
 #[derive(Debug, Clone)]
-enum Message {
+pub enum Message {
     Increment,
+    SwitchScreen(Screen),
 }
 
-enum Screen {
-    Player(),
+#[derive(Debug, Clone)]
+pub enum Screen {
+    Blah,
+    Player,
 }
 
-struct State {
+pub struct State {
     counter: u64,
+    screen: Screen,
 }
 
 fn new() -> State {
-    State { counter: 0 }
+    State {
+        counter: 0,
+        screen: Screen::Blah,
+    }
 }
 
 fn update(state: &mut State, message: Message) {
     match message {
         Message::Increment => state.counter += 1,
+        Message::SwitchScreen(screen) => state.screen = screen,
     }
 }
 
 fn view(state: &State) -> Element<'_, Message> {
-    button(text(state.counter))
-        .on_press(Message::Increment)
-        .into()
+    match state.screen {
+        Screen::Blah => {
+            container(button(text("Press me!")).on_press(Message::SwitchScreen(Screen::Player)))
+                .center_x(iced::Fill)
+                .center_y(iced::Fill)
+                .into()
+        }
+        Screen::Player => container(text("Player")).into(),
+    }
 }
 
 fn main() -> iced::Result {
